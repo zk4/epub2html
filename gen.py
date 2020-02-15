cd1605b09f497219bfa12bfa05b97d2d3c16febd
zk <liuzq7@gmail.com>
2020-02-15 20:58:46 +0800

    rc

--- gen.py
+++ gen.py
@@ -12,6 +12,7 @@ class Epub2Html():
         self.epubpath = epubpath 
         self.template =Path("./index.html").read_text()
         self.filedir = join(dirname(self.epubpath),basename(self.epubpath).split(".")[0])
+        self.absfiledir = os.path.abspath(self.filedir)
         self.textdir = os.path.join(self.filedir ,"text")
 
     
@@ -85,24 +86,20 @@ class Epub2Html():
     def gen(self):
         full_content = self.genContent()
         menu = self.genMenu()
-
-        self.template = self.template.replace("${content}$",full_content)
         self.template = self.template.replace("${menu}$",menu)
 
-        Path(join(self.filedir,"index.html")).write_text(self.template)
+        self.template = self.template.replace("${content}$",full_content)
 
+        Path(join(self.filedir,"index.html")).write_text(self.template)
+        self.copyJs()
+
+    def copyJs(self):
+        import shutil
+        dest = join(self.absfiledir,"jquery.min.js")
+        print("dest:",dest)
+        shutil.copy("/Users/zk/git/jsPrj/epubViewer/jquery.min.js",dest)
 
 
 if __name__ == "__main__":
-    # unzipFile("./a.epub")
-    # genMenuTree("./a/text/part0000.html")
-    # raw_menu =Path("./a/text/part0000.html").read_text()
-    # raw_menu = raw_menu.encode('utf-8')
-    # raw_menu_dom = etree.HTML(raw_menu)
-    # raw_menu = etree.tostring(raw_menu_dom.xpath("//body")[0],pretty_print=True).decode('utf-8')
-    # raw_menu=re.sub(r"part\w+\.html","",raw_menu)
-    # filled_template=template.replace("${menu}$",raw_menu)
-
-    # Path("./a/new_menu.html").write_text(filled_template)
     e = Epub2Html("./a.epub")
     e.gen()
