e6730c3064b2535a52bca67be6c7600291571a73
zk <liuzq7@gmail.com>
2020-02-15 18:46:50 +0800

    add

--- gen.py
+++ gen.py
@@ -1,19 +1,19 @@
 #coding: utf-8
 from lxml import etree
 from pathlib import Path
+import zipfile
 
 def parseDiv(dom,depth=0):
     titles=  dom.xpath("./a/text()")
     if len(titles)>0:
-        print(depth* "-",titles[0])
+        print(depth* "---",titles[0])
 
     divs2=  dom.xpath("./div")
     if len(divs2)>0:
         for d in divs2:
             parseDiv(d,depth+1)
 
-def genMenu(filepath):
-
+def genMenuTree(filepath):
     contents =Path(filepath).read_text()
     contents = contents.encode('utf-8')
     dom = etree.HTML(contents)
@@ -21,6 +21,17 @@ def genMenu(filepath):
     for d in divs:
         parseDiv(d)
 
+def unzipFile(filepath):
+    tokens =filepath.split(".")
+    only_name = filepath+".unzip"
+    if len(tokens)>0:
+        only_name= tokens[-2] 
+    else:
+        print("can`t extract name!")
+
+
+    with zipfile.ZipFile(filepath,'r') as zip_ref:
+        zip_ref.extractall(f"./{only_name}")
 
 def shrinkImg():
     pass
@@ -35,4 +46,7 @@ def embedContent():
 
 
 if __name__ == "__main__":
-    genMenu("./test.html")
+    unzipFile("./a.epub")
+
+    genMenuTree("./a/text/part0000.html")
+
--- xpathdemo.py
+++ xpathdemo.py
@@ -1,23 +1,3 @@
-
-from lxml import etree
-from pathlib import Path
-contents =Path('./part0000.html').read_text()
-contents = contents.encode('utf-8')
-dom = etree.HTML(contents)
-divs = dom.xpath("./body/div")
-
-def parseDiv(dom,depth=0):
-    titles=  dom.xpath("./a/text()")
-    if len(titles)>0:
-        print(depth* "-",titles[0])
-
-    divs2=  dom.xpath("./div")
-    if len(divs2)>0:
-        for d in divs2:
-            parseDiv(d,depth+1)
-
-for d in divs:
-    parseDiv(d)
-
-
-
+import zipfile
+with zipfile.ZipFile("./a.epub", 'r') as zip_ref:
+    zip_ref.extractall("./demo")
