9f24baf82e9d66be257fcb1b009a67d6c89c6b63
zk <liuzq7@gmail.com>
2020-03-01 00:04:52 +0800

    version bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 versionfile = Path("./version")
-version = versionfile.read_text()
+version = versionfile.read_text().split("\n")[0]
 [mainv,modulev,minorv] = version.split(".")
 
 VERSION = (int(mainv), int(modulev), int(minorv))
