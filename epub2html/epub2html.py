35a24244613981e350181a8fd358c5cd5852e696
zk <liuzq7@gmail.com>
2020-03-02 16:02:20 +0800

    mac use open to open browser

--- epub2html/epub2html.py
+++ epub2html/epub2html.py
@@ -205,13 +205,11 @@ def main(args):
     e = Epub2Html(filepath,outputdir)
     e.gen()
     print("converted! "+ e.getIndexLoc())
-    webbrowser.open(e.getIndexLoc(), new=2)  # open in new tab
-    # if sys.platform == "win32":
-    #     bashCommand = "start '" + e.getIndexLoc() +"'"
-    #     subprocess.check_call(bashCommand,shell=True)
-    # else:
-    #     bashCommand = "open '" + e.getIndexLoc() +"'"
-    #     subprocess.check_call(bashCommand,shell=True)
+    if sys.platform == "win32":
+        webbrowser.open(e.getIndexLoc(), new=2)  # open in new tab
+    else:
+        bashCommand = "open '" + e.getIndexLoc() +"'"
+        subprocess.check_call(bashCommand,shell=True)
 
 
 
